6cd9d7c9fac946adf3dc004ebd6ccd56f4ff01dc
Soren Zaiser <soren.zaiser@gmail.com>
2024-02-06 04:41:51 -0500

    Rollback ts-particles cuz it broke

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,161 +1,168 @@
 import Home from "./views/Home";
-import {useEffect, useState} from "preact/hooks";
-import Particles, { initParticlesEngine } from "@tsparticles/react";
-import type {Engine} from "@tsparticles/engine";
+import { useCallback, useEffect, useState } from "preact/hooks";
+import Particles from "react-tsparticles";
+import type { Engine } from "tsparticles-engine";
 import * as PC from "./data/helpers/ParticleConfigs";
-import {ParticleLoader} from "./data/helpers/ParticleConfigs";
-import {useSocket} from "./data/hooks/useSocket";
-import {Theme} from "./models/SocketMessage";
-import {loadStarsPreset} from "@tsparticles/preset-stars";
-import {loadSnowPreset} from "@tsparticles/preset-snow";
-import {loadLinksPreset} from "@tsparticles/preset-links";
-import {loadFirePreset} from "@tsparticles/preset-fire";
-import {loadHeartShape} from "@tsparticles/shape-heart";
-import {loadTextShape} from "@tsparticles/shape-text";
-import {loadFireworksPreset} from "@tsparticles/preset-fireworks";
-import {loadConfettiPreset} from "@tsparticles/preset-confetti";
-import { loadFull } from "tsparticles";
+import { ParticleLoader } from "./data/helpers/ParticleConfigs";
+import { useSocket } from "./data/hooks/useSocket";
+import { Theme } from "./models/SocketMessage";
+import { loadStarsPreset } from "tsparticles-preset-stars";
+import { loadSnowPreset } from "tsparticles-preset-snow";
+import { loadLinksPreset } from "tsparticles-preset-links";
+import { loadFirePreset } from "tsparticles-preset-fire";
+import { loadHeartShape } from "tsparticles-shape-heart";
+import { loadTextShape } from "tsparticles-shape-text";
+import { loadFireworksPreset } from "tsparticles-preset-fireworks";
+import { loadConfettiPreset } from "tsparticles-preset-confetti";
 
 export default function App() {
-
   const socket = useSocket();
   const [opts, setOpts] = useState<any>(PC.Links);
   const [loader, setLoader] = useState<ParticleLoader>(ParticleLoader.Links);
-  const [particleInit, setParticleInit] = useState<boolean>(false);
 
   useEffect(() => {
-    socket.removeListener("theme", handleTheme)
-    socket.addListener("theme", handleTheme)
+    socket.removeListener("theme", handleTheme);
+    socket.addListener("theme", handleTheme);
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-  useEffect(() => {
-    setParticleInit(false);
-    initParticlesEngine(async (engine) => {
-      await initCustomParticles(engine);
-      await loadFull(engine);
-    })
-  }, [loader]);
+  useEffect(() => {}, [loader]);
 
-  const handleTheme = ({weatherTheme, greetingTheme}: {weatherTheme: "snow" | "rain" | "clear" | "windy", greetingTheme: Theme}) => {
+  const handleTheme = ({
+    weatherTheme,
+    greetingTheme,
+  }: {
+    weatherTheme: "snow" | "rain" | "clear" | "windy";
+    greetingTheme: Theme;
+  }) => {
     console.log("Handling theme change", weatherTheme, greetingTheme);
-    if(greetingTheme === Theme.christmas) {
-      if(loader === ParticleLoader.Christmas) return;
+    if (greetingTheme === Theme.christmas) {
+      if (loader === ParticleLoader.Christmas) return;
       setLoader(ParticleLoader.Christmas);
       setOpts(PC.Christmas);
-    } else if(greetingTheme === Theme.anniversary) {
-      if(loader === ParticleLoader.Anniversary) return;
+    } else if (greetingTheme === Theme.anniversary) {
+      if (loader === ParticleLoader.Anniversary) return;
       setLoader(ParticleLoader.Anniversary);
       setOpts(PC.Anniversary);
     } else if (greetingTheme === Theme.birthday) {
-      if(loader === ParticleLoader.Birthday) return;
+      if (loader === ParticleLoader.Birthday) return;
       setLoader(ParticleLoader.Birthday);
       setOpts(PC.Birthday);
     } else if (greetingTheme === Theme.halloween) {
-      if(loader === ParticleLoader.Halloween) return;
+      if (loader === ParticleLoader.Halloween) return;
       setLoader(ParticleLoader.Halloween);
       setOpts(PC.Halloween);
-    } else if (greetingTheme === Theme.patriotic || greetingTheme === Theme.newYears) {
-      if(loader === ParticleLoader.Patriotic) return;
+    } else if (
+      greetingTheme === Theme.patriotic ||
+      greetingTheme === Theme.newYears
+    ) {
+      if (loader === ParticleLoader.Patriotic) return;
       setLoader(ParticleLoader.Patriotic);
       setOpts(PC.Patriotic);
     } else if (weatherTheme === "snow") {
-      if(loader === ParticleLoader.Snow) return;
+      if (loader === ParticleLoader.Snow) return;
       setLoader(ParticleLoader.Snow);
       setOpts(PC.Snow);
     } else if (weatherTheme === "rain") {
-      if(loader === ParticleLoader.Rain) return;
+      if (loader === ParticleLoader.Rain) return;
       setLoader(ParticleLoader.Rain);
       setOpts(PC.Rain);
     } else if (weatherTheme === "clear" && greetingTheme === Theme.night) {
-      if(loader === ParticleLoader.Stars) return;
+      if (loader === ParticleLoader.Stars) return;
       setLoader(ParticleLoader.Stars);
       setOpts(PC.Stars);
-    } else if (weatherTheme === "clear" && (greetingTheme === Theme.day || greetingTheme === Theme.morning)) {
-      if(loader === ParticleLoader.Links) return;
+    } else if (
+      weatherTheme === "clear" &&
+      (greetingTheme === Theme.day || greetingTheme === Theme.morning)
+    ) {
+      if (loader === ParticleLoader.Links) return;
       setLoader(ParticleLoader.Links);
       setOpts(PC.Links);
     } else if (weatherTheme === "windy") {
-      if(loader === ParticleLoader.Links) return;
+      if (loader === ParticleLoader.Links) return;
       setLoader(ParticleLoader.Links);
       setOpts(PC.Links);
     } else {
-      if(loader === ParticleLoader.Links) return;
+      if (loader === ParticleLoader.Links) return;
       setLoader(ParticleLoader.Links);
       setOpts(PC.Links);
     }
-  }
+  };
 
-  const initCustomParticles = async (engine: Engine) => {
-    switch (loader) {
-      case ParticleLoader.Stars:
-        await loadStarsPreset(engine);
-        await engine.load({ id: "tsParticles", options: PC.Stars });
-        break;
-      case ParticleLoader.Snow:
-        await loadSnowPreset(engine);
-        await engine.load({ id: "tsparticles", options: PC.Snow });
-        break;
-      case ParticleLoader.Links:
-        await loadLinksPreset(engine);
-        await engine.load({ id: "tsparticles", options: PC.Links });
-        break;
-      case ParticleLoader.Halloween:
-        await loadFirePreset(engine);
-        await engine.load({ id: "tsparticles", options: PC.Halloween });
-        break;
-      case ParticleLoader.Anniversary:
-        await loadHeartShape(engine);
-        await loadSnowPreset(engine);
-        await engine.load({ id: "tsparticles", options: PC.Anniversary });
-        break;
-      case ParticleLoader.Christmas:
-        await loadTextShape(engine);
-        await loadStarsPreset(engine);
-        await engine.load({ id: "tsparticles", options: PC.Christmas });
-        break;
-      case ParticleLoader.Birthday:
-        await loadTextShape(engine);
-        await loadStarsPreset(engine);
-        await engine.load({ id: "tsparticles", options: PC.Birthday });
-        break;
-      case ParticleLoader.Patriotic:
-        await loadFireworksPreset(engine);
-        await engine.load({ id: "tsparticles", options: PC.Patriotic });
-        break;
-      case ParticleLoader.Rain:
-        await loadConfettiPreset(engine);
-        await loadSnowPreset(engine);
-        await engine.load({ id: "tsparticles", options: PC.Patriotic });
-        break;
-      default:
-        await loadLinksPreset(engine);
-        await engine.load({ id: "tsparticles", options: PC.Links });
-        break;
-    }
-  }
+  const particlesInit = useCallback(
+    async (engine: Engine) => {
+      switch (loader) {
+        case ParticleLoader.Stars:
+          await loadStarsPreset(engine);
+          await engine.load("tsParticles", PC.Stars);
+          break;
+        case ParticleLoader.Snow:
+          await loadSnowPreset(engine);
+          await engine.load("tsparticles", PC.Snow);
+          break;
+        case ParticleLoader.Links:
+          await loadLinksPreset(engine);
+          await engine.load("tsparticles", PC.Links);
+          break;
+        case ParticleLoader.Halloween:
+          await loadFirePreset(engine);
+          await engine.load("tsparticles", PC.Halloween);
+          break;
+        case ParticleLoader.Anniversary:
+          await loadHeartShape(engine);
+          await loadSnowPreset(engine);
+          await engine.load("tsparticles", PC.Anniversary);
+          break;
+        case ParticleLoader.Christmas:
+          await loadTextShape(engine);
+          await loadStarsPreset(engine);
+          await engine.load("tsparticles", PC.Christmas);
+          break;
+        case ParticleLoader.Birthday:
+          await loadTextShape(engine);
+          await loadStarsPreset(engine);
+          await engine.load("tsparticles", PC.Birthday);
+          break;
+        case ParticleLoader.Patriotic:
+          await loadFireworksPreset(engine);
+          await engine.load("tsparticles", PC.Patriotic);
+          break;
+        case ParticleLoader.Rain:
+          await loadConfettiPreset(engine);
+          await loadSnowPreset(engine);
+          await engine.load("tsparticles", PC.Patriotic);
+          break;
+        default:
+          await loadLinksPreset(engine);
+          await engine.load("tsparticles", PC.Links);
+          break;
+      }
+    },
+    [loader]
+  );
 
   const style: any = {
-    height: '99vh',
-    width: '99vw',
-    overflow: 'hidden',
-    '*::-webkit-scrollbar': {
-      width: '0'
+    height: "99vh",
+    width: "99vw",
+    overflow: "hidden",
+    "*::-webkit-scrollbar": {
+      width: "0",
     },
-    '*::-webkit-scrollbar-track': {
-      '-webkit-box-shadow': 'inset 0 0 0px rgba(0,0,0,0.00)'
+    "*::-webkit-scrollbar-track": {
+      "-webkit-box-shadow": "inset 0 0 0px rgba(0,0,0,0.00)",
     },
-    '*::-webkit-scrollbar-thumb': {
-      backgroundColor: 'rgba(0,0,0,0)',
-      outline: '0px solid black'
-    }
-  }
+    "*::-webkit-scrollbar-thumb": {
+      backgroundColor: "rgba(0,0,0,0)",
+      outline: "0px solid black",
+    },
+  };
 
   return (
     <div style={style}>
-      <div style={{zIndex: -1, position: "absolute"}}>
-        {particleInit && loader && <Particles id="tsparticles" options={opts} />}
+      {/*/!* Views *!/*/}
+      <div key={loader} style={{ zIndex: -1, position: "absolute" }}>
+        <Particles id="tsparticles" options={opts} init={particlesInit} />
       </div>
-      <Home/>
+      <Home />
     </div>
   );
 }
--- frontend/src/components/WeatherIcon.tsx
+++ frontend/src/components/WeatherIcon.tsx
@@ -1,4 +1,3 @@
-import * as React from "react";
 import {
   WiAlien,
   WiCloud,
@@ -8,7 +7,11 @@ import {
   WiDust,
   WiFog,
   WiNightAltCloudy,
-  WiNightAltPartlyCloudy, WiNightAltRain, WiNightAltRainMix, WiNightAltSnow, WiNightAltStormShowers,
+  WiNightAltPartlyCloudy,
+  WiNightAltRain,
+  WiNightAltRainMix,
+  WiNightAltSnow,
+  WiNightAltStormShowers,
   WiNightClear,
   WiNightFog,
   WiRain,
@@ -18,36 +21,69 @@ import {
   WiSnow,
   WiThunderstorm,
   WiTornado,
-  WiVolcano
+  WiVolcano,
 } from "react-icons/wi";
-import {Weather} from "../models/SocketMessage";
+import { Weather } from "../models/SocketMessage";
 
-
-const WeatherIcon = ({ weather, fontSize, style }: { weather: Weather, fontSize: string, style?: React.CSSProperties }) => {
+const WeatherIcon = ({
+  weather,
+  fontSize,
+  style,
+}: {
+  weather: Weather;
+  fontSize: string;
+  style?: React.CSSProperties;
+}) => {
   const now = new Date();
   const sunrise = new Date(weather.sunrise);
   const sunset = new Date(weather.sunset);
   const night = weather.sunrise !== -1 && (now < sunrise || now > sunset);
-  switch(weather.type?.id ?? 900) {
+  switch (weather.type?.id ?? 900) {
     // Clear
     case 800:
-    case 900: return night ? <WiNightClear fontSize={fontSize} style={style} /> : <WiDaySunny fontSize={fontSize} style={style} />;
+    case 900:
+      return night ? (
+        <WiNightClear fontSize={fontSize} style={style} />
+      ) : (
+        <WiDaySunny fontSize={fontSize} style={style} />
+      );
     // Cloudy
     case 804: // Overcast
-    case 803: return night ? <WiNightAltCloudy fontSize={fontSize} style={style} /> : <WiCloudy fontSize={fontSize} style={style} />; // Broken Clouds
+    case 803:
+      return night ? (
+        <WiNightAltCloudy fontSize={fontSize} style={style} />
+      ) : (
+        <WiCloudy fontSize={fontSize} style={style} />
+      ); // Broken Clouds
     case 802: // Scattered Clouds
-    case 801: return night ? <WiNightAltPartlyCloudy fontSize={fontSize} style={style} /> : <WiCloud fontSize={fontSize} style={style} />; // Few Clouds
+    case 801:
+      return night ? (
+        <WiNightAltPartlyCloudy fontSize={fontSize} style={style} />
+      ) : (
+        <WiCloud fontSize={fontSize} style={style} />
+      ); // Few Clouds
     // Atmosphere
     case 701: // Mist
     case 761: // Dust
     case 771: // Squall
-    case 731: return <WiDust fontSize={fontSize} style={style} />; // sand/dust whirls
-    case 711: return <WiSmoke fontSize={fontSize} style={style} />; // Smoke
-    case 721: return night ? <WiNightFog fontSize={fontSize} style={style} /> : <WiDayHaze fontSize={fontSize} style={style} />; // Haze
-    case 741: return <WiFog fontSize={fontSize} style={style} />; // Fog
-    case 751: return <WiSandstorm fontSize={fontSize} style={style} />; // Sand
-    case 762: return <WiVolcano fontSize={fontSize} style={style} />; // Ash
-    case 781: return <WiTornado fontSize={fontSize} style={style} />; // Tornado
+    case 731:
+      return <WiDust fontSize={fontSize} style={style} />; // sand/dust whirls
+    case 711:
+      return <WiSmoke fontSize={fontSize} style={style} />; // Smoke
+    case 721:
+      return night ? (
+        <WiNightFog fontSize={fontSize} style={style} />
+      ) : (
+        <WiDayHaze fontSize={fontSize} style={style} />
+      ); // Haze
+    case 741:
+      return <WiFog fontSize={fontSize} style={style} />; // Fog
+    case 751:
+      return <WiSandstorm fontSize={fontSize} style={style} />; // Sand
+    case 762:
+      return <WiVolcano fontSize={fontSize} style={style} />; // Ash
+    case 781:
+      return <WiTornado fontSize={fontSize} style={style} />; // Tornado
     // Snow
     case 600: // Light Snow
     case 601: // Snow
@@ -59,18 +95,38 @@ const WeatherIcon = ({ weather, fontSize, style }: { weather: Weather, fontSize:
     case 616: // Rain and Snow
     case 620: // Light Shower Snow
     case 621: // Shower Snow
-    case 622: return night ? <WiNightAltSnow fontSize={fontSize} style={style} /> : <WiSnow fontSize={fontSize} style={style} />; // Heavy Shower Snow
+    case 622:
+      return night ? (
+        <WiNightAltSnow fontSize={fontSize} style={style} />
+      ) : (
+        <WiSnow fontSize={fontSize} style={style} />
+      ); // Heavy Shower Snow
     // Rain
     case 500: // Light Rain
     case 501: // Moderate Rain
     case 502: // Heavy Intensity Rain
     case 503: // Very Heavy Rain
-    case 504: return night ? <WiNightAltRain fontSize={fontSize} style={style} /> : <WiRain fontSize={fontSize} style={style} />; // Extreme Rain
-    case 511: return night ? <WiNightAltSnow fontSize={fontSize} style={style} /> : <WiSnow fontSize={fontSize} style={style} />; // Freezing Rain
+    case 504:
+      return night ? (
+        <WiNightAltRain fontSize={fontSize} style={style} />
+      ) : (
+        <WiRain fontSize={fontSize} style={style} />
+      ); // Extreme Rain
+    case 511:
+      return night ? (
+        <WiNightAltSnow fontSize={fontSize} style={style} />
+      ) : (
+        <WiSnow fontSize={fontSize} style={style} />
+      ); // Freezing Rain
     case 520: // Light Intensity Shower Rain
     case 521: // Shower Rain
     case 522: // Heavy Intensity Shower Rain
-    case 531: return night ? <WiNightAltRainMix fontSize={fontSize} style={style} /> : <WiRainMix fontSize={fontSize} style={style} />; // Ragged Shower Rain
+    case 531:
+      return night ? (
+        <WiNightAltRainMix fontSize={fontSize} style={style} />
+      ) : (
+        <WiRainMix fontSize={fontSize} style={style} />
+      ); // Ragged Shower Rain
     // Drizzle
     case 300: // Light Intensity Drizzle
     case 301: // Drizzle
@@ -80,7 +136,12 @@ const WeatherIcon = ({ weather, fontSize, style }: { weather: Weather, fontSize:
     case 312: // Heavy Intensity Drizzle Rain
     case 313: // Shower Rain and Drizzle
     case 314: // Heavy Shower Rain and Drizzle
-    case 321: return night ? <WiNightAltRain fontSize={fontSize} style={style} /> : <WiRain fontSize={fontSize} style={style} />; // Shower Drizzle
+    case 321:
+      return night ? (
+        <WiNightAltRain fontSize={fontSize} style={style} />
+      ) : (
+        <WiRain fontSize={fontSize} style={style} />
+      ); // Shower Drizzle
     // Thunderstorm
     case 200: // Thunderstorm with Light Rain
     case 201: // Thunderstorm with Rain
@@ -91,10 +152,16 @@ const WeatherIcon = ({ weather, fontSize, style }: { weather: Weather, fontSize:
     case 221: // Ragged Thunderstorm
     case 230: // Thunderstorm with Light Drizzle
     case 231: // Thunderstorm with Drizzle
-    case 232: return night ? <WiNightAltStormShowers fontSize={fontSize} style={style} /> : <WiThunderstorm fontSize={fontSize} style={style} />; // Thunderstorm with Heavy Drizzle
+    case 232:
+      return night ? (
+        <WiNightAltStormShowers fontSize={fontSize} style={style} />
+      ) : (
+        <WiThunderstorm fontSize={fontSize} style={style} />
+      ); // Thunderstorm with Heavy Drizzle
 
-    default: return <WiAlien fontSize={fontSize} style={style} />; // Unknown?
+    default:
+      return <WiAlien fontSize={fontSize} style={style} />; // Unknown?
   }
-}
+};
 
 export default WeatherIcon;
